38d526213d3f888ea989b1cac4ffe4804cdda4c3
wencheng <2515106327@qq.com>
2019-02-14 00:49:57 +0800

    second commit 实现文件上传 放置用户名+日期文件夹内

--- src/com/cdtu/servlet/UploadServlet.java
+++ src/com/cdtu/servlet/UploadServlet.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -63,6 +66,8 @@ public class UploadServlet extends HttpServlet {
 	private void processFormUpload(FileItem fileitem) {
 		//得到文件名称
 		String filename = fileitem.getName();//‪C:\Users\Administrator\Pictures\1.jpg 或者1.jpg
+		//避免文件名重复
+		filename=UUID.randomUUID()+filename;
 		//处理文件名 
 		if(filename!=null){
 			//filename=filename.substring(filename.lastIndexOf(File.separator)+1);
@@ -77,7 +82,10 @@ public class UploadServlet extends HttpServlet {
 			if(!storeFile.exists()){
 				storeFile.mkdirs();
 			}
-			File file = new File(storeFile,filename);
+			String username ="文成";
+			//按用户+日期分配目录
+			String childDirectory = makeChildDirectory(storeFile,username);
+			File file = new File(storeFile,childDirectory+File.separator+filename);
 			FileOutputStream fileOutputStream = new FileOutputStream(file);
 			int len=0;
 			byte [] b = new byte[1024];
@@ -91,6 +99,16 @@ public class UploadServlet extends HttpServlet {
 		}
 		
 	}
+	private String makeChildDirectory(File storeFile, String username) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String dataDirectory = username+File.separator+sdf.format(new Date());		
+		File childDirectory = new File(storeFile,dataDirectory);
+		if(!childDirectory.exists()){
+			childDirectory.mkdirs();
+		}
+		return dataDirectory;
+	}
+
 	//普通表单
 	private void processFormField(FileItem fileItem) {
 		String fieldName = fileItem.getFieldName();
