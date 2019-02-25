a4eec57e49f87c04ead11258dedcaa8f7d4ffd38
wencheng <2515106327@qq.com>
2019-02-25 16:00:10 +0800

    forth comit

---
+++ src/com/cdtu/servlet/test.java
@@ -0,0 +1,18 @@
+package com.cdtu.servlet;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer t1=200;
+		Integer t2=200;
+		System.out.println(t1==t2);
+		System.out.println(t1.equals(t2));
+		
+		Integer a=150;
+		int b=150;
+		System.out.println(a==b);
+		System.out.println(a.equals(b));
+	}
+
+}
